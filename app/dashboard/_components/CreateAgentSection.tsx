51267cde902ff8dc3cc98f4ff8a2e86ed35d0c6c
Sandeep-singh-99 <sandeep.necoder@gmail.com>
2025-11-19 16:40:24 +0530

    feat: enhance CreateAgentSection with loading state and redirect after agent creation

--- app/dashboard/_components/CreateAgentSection.tsx
+++ app/dashboard/_components/CreateAgentSection.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { Button } from "@/components/ui/button";
-import { Plus } from "lucide-react";
+import { Loader, Plus } from "lucide-react";
 import React, { useState } from "react";
 import {
   Dialog,
@@ -13,9 +13,31 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
+import { useMutation } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import { v4 as uuidv4 } from "uuid";
+import { useRouter } from "next/navigation";
 
 export default function CreateAgentSection() {
   const [openDialog, setOpenDialog] = useState(false);
+
+  const CreateAgentMutation = useMutation(api.agent.CreateAgent);
+  const [agentName, setAgentName] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const router = useRouter();
+
+  const CreateAgent = async () => {
+    setLoading(true);
+    const agentId = uuidv4();
+    const results = await CreateAgentMutation({
+      name: agentName ?? '',
+      agentId: agentId,
+    })
+    setOpenDialog(false);
+    setLoading(false);
+    router.push(`/agent-builder/${agentId}`);
+  }
   return (
     <div className="space-y-2 flex flex-col justify-center items-center mt-24">
       <h2 className="font-bold text-3xl">Create AI Agent</h2>
@@ -31,7 +53,7 @@ export default function CreateAgentSection() {
           <DialogHeader>
             <DialogTitle>Enter Agent Name</DialogTitle>
             <DialogDescription>
-              <Input placeholder="Agent Name" />
+              <Input placeholder="Agent Name" value={agentName} onChange={(e) => setAgentName(e.target.value)} />
             </DialogDescription>
           </DialogHeader>
           <DialogFooter>
@@ -40,7 +62,10 @@ export default function CreateAgentSection() {
                 Close
               </Button>
             </DialogClose>
-            <Button type="submit">Create Agent</Button>
+            <Button type="submit" onClick={() => CreateAgent()} disabled={loading}>
+              { loading && <Loader className="animate-spin" />}
+              Create Agent
+              </Button>
           </DialogFooter>
         </DialogContent>
       </Dialog>
