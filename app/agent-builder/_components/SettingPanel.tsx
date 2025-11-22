a4e8d6294cc914f27406256f7641b0326a78fbcf
Sandeep-singh-99 <sandeep.necoder@gmail.com>
2025-11-22 15:53:13 +0530

    feat: Implement new UI components (Label, Select, Switch, Textarea) and agent settings panel.

--- app/agent-builder/_components/SettingPanel.tsx
+++ app/agent-builder/_components/SettingPanel.tsx
@@ -1,12 +1,25 @@
 import { WorkflowContext } from '@/context/WorkflowContext'
 import React, { useContext } from 'react'
+import AgentSettings from '../_nodeSettings/AgentSettings';
 
 export default function SettingPanel() {
-    const { selectedNode } = useContext(WorkflowContext);
-    
-  return (
-    <div className='p-5 bg-white rounded-2xl w-[350px] shadow'>
+    const { selectedNode, setAddedNodes } = useContext(WorkflowContext);
 
+    const onUpdateNodeData = (formData: any) => {
+        const updateNode = {
+            ...selectedNode,
+            data: {
+                ...selectedNode.data,
+                label: formData.name,
+                settings: formData
+            }
+        }
+        setAddedNodes((prev: any) => prev.map((node: any) => (node.id === selectedNode.id ? updateNode : node)))
+    }
+
+  return selectedNode && (
+    <div className='p-5 bg-white rounded-2xl w-[350px] shadow'>
+        {selectedNode?.type === 'AgentNode' && <AgentSettings selectedNode={selectedNode} updateFormData={(value: any) => onUpdateNodeData(value)} />}
     </div>
   )
 }
